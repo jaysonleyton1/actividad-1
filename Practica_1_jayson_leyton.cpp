204652448700e2ee94113ba294f839a20c031e5c
jaysonleyton1 <jaysonleyton1@gmail.com>
2016-09-05 20:01:15 -0500

    VERSION 1
    Sitio web inicial

---
+++ Practica_1_jayson_leyton.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <stdio.h> 
+#include<conio.h>
+
+using namespace std;
+ 
+void pausa();
+ 
+int main()
+{
+    
+
+
+        system("cls");
+        cin.clear();
+        cout << "\n  -------------------------------------------------------------------\n";
+		cout<<"\n                       B I E N V E N I D O S  ";
+		cout<<"\n           UNIVERSIDAD NACIONAL ABIERTA Y A DISTANCIA UNAD";
+		cout<<"\n                        DISEÑO DE SITIOS WEBS";
+ 	    cout << "\n  -------------------------------------------------------------------\n";
+		cout<<"\n";
+		cout<<"\nHora  ";system("time /t");cout<<"\Fecha  ";system("date /t");//fecha y hora actual--- ok 
+		cout<<"    CURSO------->>>>          DISEÑO DE SITIOS WEBS"<<endl;
+        cout<<"    TUTOR------->>>>          MAURICIO PERDOMO VARGAS"<<endl;
+        cout<<"    ALUMNO------>>>>          JAYSON ANDRES LEYTON SARMIENTO"<<endl; 
+        cout<<"    PROGRAMA---->>>>          INGENIERIA DE SISTEMAS"<<endl; 
+        cout<<"    CEAD-------->>>>          FACATATIVA"<<endl;
+        cout<<"    E-MAIL------>>>>          jaysonleyton1@gmail.com"<<endl;
+        cout<<"    SKYPE------->>>>          andresleyton1988"<<endl;
+        cout<<"    CELULAR----->>>>          3133627314"<<endl<<endl<<endl;
+			
+
+			
+}
+ 
+			
